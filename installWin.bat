02a017c0be16b6f4bcb5c0cd0222d76eda6a9870
Danny Takushi <okinawanspud@gmail.com>
2023-10-13 21:15:42 -0500

    organize & add a few things

--- installWin.bat
+++ installWin.bat
@@ -1,13 +1,23 @@
-winget install -e --id Git.Git
-winget install -e --id Docker.DockerDesktop
+REM essentials
+winget install -e --id Brave.Brave
 winget install -e --id evernote.evernote
+
+REM coding
+winget install -e --id Git.Git
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Python.Python.3
+winget install -e --id WinMerge.WinMerge
+winget install -e --id Docker.DockerDesktop
+
+REM graphics
 winget install -e --id flux.flux
 winget install -e --id GIMP.GIMP
-winget install -e --id GitHub.GitHubDesktop
+
+REM messaging
 winget install -e --id LINE.LINE
-winget install -e --id Python.Python.3
 winget install -e --id Telegram.TelegramDesktop
 winget install -e --id WhatsApp.WhatsApp
-winget install -e --id WinMerge.WinMerge
-winget install -e --id evernote.evernote
+
+REM files support
 winget install -e --id Dropbox.Dropbox
