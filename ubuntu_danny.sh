193baf24c4d8a0a75eddd9584470853fb05ac1d3
Danny Takushi <okinawanspud@gmail.com>
2023-10-23 11:24:35 -0500

    Create ubuntu_danny.sh

---
+++ ubuntu_danny.sh
@@ -0,0 +1,42 @@
+sudo apt install -y \
+  curl \
+  git \
+  gparted \
+  python3-distutils \
+  python3-pip
+pip3 install --upgrade pip
+python3 -m pip install mysql-connector
+
+sudo snap install \
+  --classic code \
+  firefox \
+  sublime \
+  postman
+
+# Radio
+# https://github.com/gqrx-sdr/gqrx
+git clone https://github.com/gqrx-sdr/gqrx.git gqrx.git
+cd gqrx.git
+mkdir build
+cd build
+cmake ..
+make install
+sudo ldconfig
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo bash -c 'echo "blacklist dvb_usb_rtl28xxu" >> \
+  /etc/modprobe.d/blacklist-rtl.conf'
+
+# Edge for teams (chromium seems to not work nicely with Brave, and --google)
+# https://www.omgubuntu.co.uk/2021/01/how-to-install-edge-on-ubuntu-linux
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo rm microsoft.gpg
+sudo apt update && sudo apt install microsoft-edge-stable
+
+# Brave from official instructions
+# https://brave.com/linux/
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
