36112e1fbd80b4fc34fb63201383679d1244c05f
Danny Takushi <okinawanspud@gmail.com>
2024-01-31 15:15:51 -0600

    docker content

--- ubuntu_danny.sh
+++ ubuntu_danny.sh
@@ -60,3 +60,22 @@ git config user.name "danny"
 # Check out this thread: https://askubuntu.com/questions/1407494/screen-share-not-working-in-ubuntu-22-04-in-all-platforms-zoom-teams-google-m 
 ## First option worked, but X11 performed poorly on my hardware setup (4k monitor)
 ## vr4u's comment on enabling Additional Drivers worked perfectly for me
+
+
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# To install the latest version, run:
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
