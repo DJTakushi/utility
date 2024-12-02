df35511b1a80095b960bb8fa6ad1de684fe5e77a
danny <dannytakushi@gmail.com>
2024-12-02 13:34:43 -0600

    docker group creation

--- ubuntu_danny.sh
+++ ubuntu_danny.sh
@@ -67,13 +67,16 @@ git config --global user.name "danny"
 # sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 # sudo chmod a+r /etc/apt/keyrings/docker.asc
 sudo snap install docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
 # Add the repository to Apt sources:
 # echo \
 #   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
 #   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 #   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+# sudo apt-get update
 
 # To install the latest version, run:
 # sudo aptd-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
