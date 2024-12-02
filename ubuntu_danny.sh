837a63f2b4170f390e1cce7f9c94530c7d38ebcb
danny <dannytakushi@gmail.com>
2024-12-02 17:06:16 -0600

    remove edge

--- ubuntu_danny.sh
+++ ubuntu_danny.sh
@@ -43,11 +43,11 @@ sudo apt install gqrx-sdr
 
 # Edge for teams (chromium seems to not work nicely with Brave, and --google)
 # https://www.omgubuntu.co.uk/2021/01/how-to-install-edge-on-ubuntu-linuxsudo apt update && sudo apt install microsoft-edge-stable
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-sudo rm microsoft.gpg
-sudo apt update && sudo apt install microsoft-edge-stable
+# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+# sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+# sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+# sudo rm microsoft.gpg
+# sudo apt update && sudo apt install microsoft-edge-stable
 
 git config --global user.email "dannytakushi@gmail.com"
 git config --global user.name "danny"
